fcd52c591ea830ee4ffe8c722a7149c6088b2240
Atik <atik@flightexpert.com>
2025-11-22 18:21:21 +0600

    fix

--- database/migrations/2025_10_24_095448_create_product_images_table.php
+++ database/migrations/2025_10_24_095448_create_product_images_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained('products', 'product_id')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('path', 255);
             $table->integer('sort_order')->default(0);
             $table->timestamps();
